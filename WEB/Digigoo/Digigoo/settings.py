72a8e3c073055b3b4cccce12aaf6471a7967a5d8
JeongChanHwi <bbs4614@naver.com>
2021-12-30 15:42:27 +0900

    makeshift

--- WEB/Digigoo/Digigoo/settings.py
+++ WEB/Digigoo/Digigoo/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [    BASE_DIR / "static",    '/var/www/static/',]
+STATICFILES_DIRS = [    BASE_DIR / "static", ]
 STATICFILES_FINDERS = ['django.contrib.staticfiles.finders.FileSystemFinder',]
 IMAGE_URL = '/image/'
 IMAGE_ROOT = os.path.join(BASE_DIR, 'image')

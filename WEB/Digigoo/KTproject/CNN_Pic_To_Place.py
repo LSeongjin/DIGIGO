db5b0013875f3ebaf8156969d7d00e9a61cf29e3
urrburu <bbs4614@naver.com>
2022-01-02 15:59:06 +0900

    CNN and GAN

--- WEB/Digigoo/KTproject/CNN_Pic_To_Place.py
+++ WEB/Digigoo/KTproject/CNN_Pic_To_Place.py
@@ -1,4 +1,22 @@
+import tensorflow as tf
+import numpy as np
+import cv2
 
-def Pic_to_place(Img):
-    #Img.save('GANed_Image.png', 'PNG')
-    return [['GyongPoDae','99.8'], ['HaeUnDae','70.3'], ['JungDongJin','30.5']]
+def Pic_to_place(ID):
+
+    label = {0: "Mountain", 1: "Ocean", 2: "Tower", 3: "Plain"}
+    label_num = 4
+
+    cnn_model = tf.keras.models.load_model("cnnmodel_211230")
+    #image_file = ID+'GANed_Image.png'
+    image_file = "image/"+str(ID)+"GANed_Image.png"
+    img = np.array(np.array([cv2.resize(cv2.imread(image_file, cv2.IMREAD_COLOR), dsize=(256, 256),
+                                        interpolation=cv2.INTER_LINEAR).astype(np.float64)])) / (256.0)
+    result = np.array(cnn_model(img))
+
+    probability = []
+    for i in range(label_num):
+        probability.append([str(round(100 * result[0][i], 2)), label[i]])
+        #print(label[i], str(round(100 * result[0][i], 2)))
+        probability.sort(reverse=True)
+    return probability
--- WEB/Digigoo/KTproject/GAN_Drawn_To_Take_Picture.py
+++ WEB/Digigoo/KTproject/GAN_Drawn_To_Take_Picture.py
@@ -1,3 +1,46 @@
-import PIL
-def GAN_Drawn_To_Take_Picture(Img, choice):
-    return Img
+import tensorflow as tf
+import numpy as np
+def GAN_Drawn_To_Take_Picture(IMG, choice, ID):
+    # IMG : np.array(256, 256, 3)
+
+    # Data preprocessing
+
+    IMG = tf.cast(IMG, tf.float32)
+    IMG = tf.stack([IMG], axis=0)
+    IMG = (IMG / 127.5) - 1
+
+    if choice == "OC":
+        model_sea = tf.keras.models.load_model("GAN_sea_Model_epoch_v1_100.h5")
+        prediction = model_sea(IMG, training=True)
+        tf.keras.utils.save_img("image/"+str(ID) + "GANed_Image.png", np.array(prediction[0]), data_format="channels_last",
+                                file_format=None, scale=True)  # save image at output_folder for web
+        return np.array(prediction[0]*0.5 +0.5)  # np.array(255, 255, 3)
+
+    if choice == "LM":
+        model_tower = tf.keras.models.load_model("GAN_Tower_Model_epoch_v2_150.h5")
+        prediction = model_tower(IMG, training=True)
+        tf.keras.utils.save_img("image/"+str(ID) + "GANed_Image.png", np.array(prediction[0]), data_format="channels_last",
+                                file_format=None, scale=True)  # save image at output_folder for web
+        return np.array(prediction[0]*0.5 +0.5)
+
+    if choice == "MT":
+        model_mountain = tf.keras.models.load_model("GAN_Mountain_Model_epoch_v1_150.h5")
+        prediction = model_mountain(IMG, training=True)
+        tf.keras.utils.save_img("image/"+str(ID) + "GANed_Image.png", np.array(prediction[0]), data_format="channels_last",
+                                file_format=None, scale=True)  # save image at output_folder for web
+        return np.array(prediction[0]*0.5 +0.5)
+
+    if choice == "PL":
+        model_flame = tf.keras.models.load_model("GAN_Flame_Model_epoch210.h5")
+        prediction = model_flame(IMG, training=True)
+        tf.keras.utils.save_img("image/"+str(ID) + "GANed_Image.png", np.array(prediction[0]), data_format="channels_last",
+                                file_format=None, scale=True)  # save image at output_folder for web
+        return np.array(prediction[0]*0.5 +0.5)
+
+    else:
+        print("error : " + choice)
+
+        return IMG
+
+    tf.keras.utils.save_img( str(ID)+"GANed_Image.png", np.array(prediction[0]), data_format="channels_last",
+                            file_format=None, scale=True)  # save image at output_folder for web
